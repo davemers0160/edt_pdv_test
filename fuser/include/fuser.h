cff9298e144e76cc4a738218a14a30f3b6ff748b
Dave <davemers@iu.edu>
2021-09-08 23:13:40 -0400

    checking the lib linking

--- fuser/include/fuser.h
+++ fuser/include/fuser.h
@@ -2,95 +2,37 @@
 #define _FUSER_HEADER_H
 
 #include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
+//#include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-#include <opencv2/features2d.hpp>
-#include <opencv2/xfeatures2d.hpp>
-#include <opencv2/calib3d.hpp>
 
-const int MAX_FEATURES = 300;
-const float GOOD_MATCH_PERCENT = 0.10f;
-
-// ----------------------------------------------------------------------------
-typedef struct mouse_points
+//-----------------------------------------------------------------------------
+typedef struct ms_image
 {
-    int id;
-    std::vector<cv::Point2f> points;
-} mouse_points;
+    double* image;
 
+    unsigned int img_w;
+    unsigned int img_h;
 
-// ----------------------------------------------------------------------------
-void find_transformation_matrix(cv::Mat& img1, cv::Mat& img2, cv::Mat& h, cv::Mat &img_matches)
+    bool use_img = true;
+    bool invert_img = false;
+
+    double weight = 0.5;
+
+} ms_image;
+
+//-----------------------------------------------------------------------------
+inline ms_image init_ms_image(double* image, unsigned int img_w, unsigned int img_h, bool use_img, bool invert_img, double weight)
 {
-
-    // Variables to store keypoints and descriptors
-    std::vector<cv::KeyPoint> kp1, kp2;
-    cv::Mat descriptors1, descriptors2;
-
-    // Detect ORB features and compute descriptors.
-    cv::Ptr<cv::Feature2D> orb = cv::ORB::create(MAX_FEATURES, 1.2, 8, 25, 0, 4, cv::ORB::HARRIS_SCORE, 25);
-    orb->detectAndCompute(img1, cv::Mat(), kp1, descriptors1);
-    orb->detectAndCompute(img2, cv::Mat(), kp2, descriptors2);
-
-    // Match features.
-    std::vector<cv::DMatch> matches;
-    //cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create("BruteForce-Hamming");
-    cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::MatcherType::BRUTEFORCE_HAMMING);
-    matcher->match(descriptors1, descriptors2, matches, cv::Mat());
-
-    // Sort matches by score
-    std::sort(matches.begin(), matches.end());
-
-    // Remove not so good matches
-    const int numGoodMatches = (int)ceil(matches.size() * GOOD_MATCH_PERCENT);
-    matches.erase(matches.begin() + numGoodMatches, matches.end());
-
-    // Draw top matches
-    cv::drawMatches(img1, kp1, img2, kp2, matches, img_matches);
-    //imwrite("matches.jpg", imMatches);
-
-    // Extract location of good matches
-    std::vector<cv::Point2f> points1, points2;
-
-    for (size_t i = 0; i < matches.size(); i++)
-    {
-        points1.push_back(kp1[matches[i].queryIdx].pt);
-        points2.push_back(kp2[matches[i].trainIdx].pt);
-    }
-
-    // Find homography
-    h = cv::findHomography(points1, points2, cv::RANSAC);
-
-    // Use homography to warp image
-    //warpPerspective(img1, im1Reg, h, img2.size());
+    ms_image tmp;
+    tmp.image = image;
+    tmp.img_w = img_w;
+    tmp.img_h = img_h;
+    tmp.use_img = use_img;
+    tmp.invert_img = invert_img;
+    tmp.weight = weight;
+    return tmp;
 }
-
-// ----------------------------------------------------------------------------
-cv::Mat get_gradient(cv::Mat &src)
-{
-    cv::Mat grad_x, grad_y;
-    cv::Mat abs_grad_x, abs_grad_y;
-
-    int scale = 1;
-    int delta = 0;
-    int ddepth = CV_32FC1; ;
-
-    // Calculate the x and y gradients using Sobel operator
-    cv::Sobel(src, grad_x, ddepth, 1, 0, 3, scale, delta, cv::BORDER_DEFAULT);
-    cv::convertScaleAbs(grad_x, abs_grad_x);
-
-    cv::Sobel(src, grad_y, ddepth, 0, 1, 3, scale, delta, cv::BORDER_DEFAULT);
-    cv::convertScaleAbs(grad_y, abs_grad_y);
-
-    // Combine the two gradients
-    cv::Mat grad;
-    cv::addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
-
-    return grad;
-
-}
-
 
 // ----------------------------------------------------------------------------
 void generate_checkerboard(uint32_t block_w, uint32_t block_h, uint32_t img_w, uint32_t img_h, cv::Mat& checker_board)
@@ -124,18 +66,5 @@ void generate_checkerboard(uint32_t block_w, uint32_t block_h, uint32_t img_w, u
     checker_board = checker_board(roi);
 }
 
-// ----------------------------------------------------------------------------
-void cv_mouse_click(int cb_event, int x, int y, int flags, void* param) 
-{
-    if (cb_event == cv::EVENT_LBUTTONDOWN)
-    {
-        std::vector<cv::Point2f>* point = (std::vector<cv::Point2f>*)param;
-
-        point->push_back(cv::Point2f(x, y));
-
-        std::cout << "Point(" << x << ", " << y << ")" << std::endl;
-    }
-
-}
 
 #endif	// _FUSER_HEADER_H
--- fuser/src/fuser_main.cpp
+++ fuser/src/fuser_main.cpp
@@ -1,3 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
+#include <windows.h>
+#else
+#include <dlfcn.h>
+#endif
 
 #include <cstdint>
 #include <algorithm>
@@ -9,111 +16,80 @@
 #include <opencv2/imgproc.hpp>
 
 #include "fuser.h"
-//#include "image_proc.h"
-//#include "camera.h"
-//#include "ms_tracker.h"
-//#include "target.h"
 
-// additional includes for external libraries
-// #include "select_roi.h"
-// #include "ms_tracker_lib.h"
-
-//
-// ----------------------------------------------------------------------------
-typedef struct ms_image
-{
-    double* image;
-
-    unsigned int img_w;
-    unsigned int img_h;
-
-    bool use_img = true;
-    bool invert_img = false;
-
-    double weight = 0.5;
-
-} ms_image;
-
-inline ms_image init_ms_image(double* image, unsigned int img_w, unsigned int img_h, bool use_img, bool invert_img, double weight)
-{
-    ms_image tmp;
-    tmp.image = image;
-    tmp.img_w = img_w;
-    tmp.img_h = img_h;
-    tmp.use_img = use_img;
-    tmp.invert_img = invert_img;
-    tmp.weight = weight;
-    return tmp;
-}
-
-// ----------------------------------------------------------------------------
-void image_fuser(unsigned int num_images, ms_image* img, double* fused_data64_t, unsigned char* fused_data8_t, unsigned int img_w, unsigned int img_h)
-{
-    unsigned int idx;
-
-    // assign the fused data pointer to an opencv container
-    cv::Mat fused_img = cv::Mat(img_h, img_w, CV_64FC1, fused_data64_t);
-    cv::Mat fused_img8_t = cv::Mat(img_h, img_w, CV_8UC1, fused_data8_t);
-    cv::Mat tmp_img;
-
-    for (idx = 0; idx < num_images; ++idx)
-    {
-        if (img[idx].use_img)
-        {
-            tmp_img = cv::Mat(img[idx].img_h, img[idx].img_w, CV_64FC1, img[idx].image);
-            fused_img = fused_img + img[idx].weight * (img[idx].invert_img ? (1.0 - tmp_img) : tmp_img);
-        }
-    }
-
-    fused_img.convertTo(fused_img8_t, CV_8UC1, 255.0, 0.0);
-
-}   // end of image_fuser
-
+typedef void (*image_fuser)(unsigned int num_images, ms_image* img, double* fused_data64_t, unsigned char* fused_data8_t, unsigned int img_w, unsigned int img_h);
 
 // ----------------------------------------------------------------------------
 int main()
 {
     uint32_t idx;
-    std::vector<bool> use_layer = { true, true, false};
-    std::vector<bool> invert_layer = {false, false, false};
+    //std::vector<bool> use_layer = { true, true, false};
+    //std::vector<bool> invert_layer = {false, false, false};
     
-    std::vector<cv::Mat> layers;
-    std::vector<std::string> img_pathes = { "e:/data/lwir_0001a.png","e:/data/mwir_0001b.png" };
+    //std::vector<cv::Mat> layers;
+    //std::vector<std::string> img_pathes = { "e:/data/lwir_0001a.png","e:/data/mwir_0001b.png" };
 
-    std::vector<double> layer_weight = { 0.5, 0.5, 0.1 };
+    //std::vector<double> layer_weight = { 0.5, 0.5, 0.1 };
 
-    std::vector<cv::Point2f> alignment_points1;
-    std::vector<cv::Point2f> alignment_points2;
+    //std::vector<cv::Point2f> alignment_points1;
+    //std::vector<cv::Point2f> alignment_points2;
+
+    std::string lib_filename;
 
     // create the registration matrix to try and register the two images 
-    cv::Mat h;
-    cv::Mat tmp_reg;
-
+    //cv::Mat h;
+    //cv::Mat tmp_reg;
+    cv::Mat cb1, cb2, cb3;
 
     std::string window_name1 = "Base Image";
-    std::string window_name2 = "Second Image";
+    //std::string window_name2 = "Second Image";
+
 
     cv::namedWindow(window_name1);// , cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO);
-    cv::namedWindow(window_name2);// , cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO);
-    cv::namedWindow("fused");
+    //cv::namedWindow(window_name2);// , cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO);
+    //cv::namedWindow("fused");
+
+
+    // load in the library
+#if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
+    lib_filename = "../../fusion_lib/build/Release/ms_fuser.dll";
+    HINSTANCE obj_det_lib = LoadLibrary(lib_filename.c_str());
+
+    if (obj_det_lib == NULL)
+    {
+        throw std::runtime_error("error loading library");
+    }
+
+    image_fuser lib_image_fuser = (image_fuser)GetProcAddress(obj_det_lib, "image_fuser");
+
+#else
+    void* obj_det_lib = dlopen(lib_filename.c_str(), RTLD_NOW);
+
+    if (obj_det_lib == NULL)
+    {
+        throw std::runtime_error("error loading library");
+    }
+
+    image_fuser lib_image_fuser = (image_fuser)dlsym(obj_det_lib, "image_fuser");
+
+#endif
 
     int bp = 0;
 
     // read in the image
-    layers.clear();
-    for (idx = 0; idx < img_pathes.size(); ++idx)
-    {
-        cv::Mat tmp = cv::imread(img_pathes[idx], cv::ImreadModes::IMREAD_GRAYSCALE);
-        tmp.convertTo(tmp, CV_32FC1, 1.0 / 255.0, 0.0);
-        layers.push_back(tmp);
-    }
+    //layers.clear();
+    //for (idx = 0; idx < img_pathes.size(); ++idx)
+    //{
+    //    cv::Mat tmp = cv::imread(img_pathes[idx], cv::ImreadModes::IMREAD_GRAYSCALE);
+    //    tmp.convertTo(tmp, CV_32FC1, 1.0 / 255.0, 0.0);
+    //    layers.push_back(tmp);
+    //}
     
     
     //cv::Mat fused_img = cv::Mat(layers[0].rows, layers[0].cols, CV_32FC1, cv::Scalar::all(0.0));
     //fused_img = fused_img + layer_weight[0] * (invert_layer[0] ? (1.0 - layers[0]) : layers[0]);
 
-    cv::Mat checker_board, cb1, cb2;
-    generate_checkerboard(32, 32, layers[1].cols, layers[1].rows, checker_board);
+    //generate_checkerboard(32, 32, layers[1].cols, layers[1].rows, checker_board);
 
     /*
     layers.push_back(checker_board);
@@ -172,26 +148,12 @@ int main()
     }
     */
 
-
-    // read in the layer weights
-    
-    
-    // read in the use_layer option
-    
-
-    // read in the invert_layer option
-    
-    
-
     //find_transformation_matrix(get_gradient(layers[1]), get_gradient(layers[0]), h, img_matches);
 
     //cv::drawMatches(layers[0], alignment_points1, layers[1], alignment_points2, matches, img_matches);
 
     // Find homography
     //h = cv::findHomography(alignment_points2, alignment_points1, cv::RANSAC);
-
-
-
 
     // apply registration to images
     //std::vector<cv::Mat> register_img;
@@ -215,36 +177,22 @@ int main()
 
     generate_checkerboard(32, 32, img_w, img_h, cb1);
     generate_checkerboard(64, 64, img_w, img_h, cb2);
+    generate_checkerboard(16, 16, img_w, img_h, cb3);
 
     cb1.convertTo(cb1, CV_64FC1, 1.0 / 255.0, 0.0);
     cb2.convertTo(cb2, CV_64FC1, 1.0 / 255.0, 0.0);
+    cb3.convertTo(cb3, CV_64FC1, 1.0 / 255.0, 0.0);
 
     cv::Mat fused_img = cv::Mat::zeros(img_h, img_w, CV_64FC1);
     cv::Mat fused_img8_t = cv::Mat::zeros(img_h, img_w, CV_8UC1);
 
-    ms_image tmp1 = init_ms_image(cb1.ptr<double>(0), img_w, img_h, true, false, 0.75);
-    ms_image tmp2 = init_ms_image(cb2.ptr<double>(0), img_w, img_h, true, false, 0.25);
+    ms_image tmp1 = init_ms_image(cb1.ptr<double>(0), img_w, img_h, true, false, 0.6);
+    ms_image tmp2 = init_ms_image(cb2.ptr<double>(0), img_w, img_h, true, false, 0.3);
+    ms_image tmp3 = init_ms_image(cb3.ptr<double>(0), img_w, img_h, true, false, 0.1);
 
-    std::vector<ms_image> tmp_ms = { tmp1, tmp2 };
+    std::vector<ms_image> tmp_ms = { tmp1, tmp2, tmp3 };
 
-    image_fuser(tmp_ms.size(), tmp_ms.data(), fused_img.ptr<double>(0), fused_img8_t.ptr<uint8_t>(0), img_w, img_h);
-
-    //layers[1] = im1Reg.clone();
-
-
-    //for (idx = 0; idx < img_pathes.size(); ++idx)
-    //{
-    //    if (use_layer[idx])
-    //        fused_img = fused_img + layer_weight[idx] * (invert_layer[idx] ? (1.0 - layers[idx]) : layers[idx]);
-
-    //}
-
-    //fused_img.convertTo(fused_img, CV_8UC3, 255.0, 0.0);
-
-    //cv::Mat t3 = cv::Mat::zeros(img_h, img_w, CV_8UC3);
-    //fused_img.convertTo(fused_img, CV_8UC3, 255.0, 0.0);
-
-    //cv::cvtColor(fused_img*255, fused_img, cv::COLOR_GRAY2RGB);
+    lib_image_fuser(tmp_ms.size(), tmp_ms.data(), fused_img.ptr<double>(0), fused_img8_t.ptr<uint8_t>(0), img_w, img_h);
 
     // display results
     cv::imshow(window_name1, fused_img);
