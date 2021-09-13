70d8d9452d2967a7c92b58c6024b48ddc16f6cd2
david emerson <davemers@iu.edu>
2021-09-13 13:01:41 -0400

    adding center cropping into the mix

--- fusion_lib/src/fusion_lib.cpp
+++ fusion_lib/src/fusion_lib.cpp
@@ -29,10 +29,20 @@ void image_fuser(unsigned int num_images, ms_image* img, double* fused_data64_t,
     {
         if (img[idx].use_img)
         {
+            // get the pointer data into a cv::Mat container
             tmp_img = cv::Mat(img[idx].img_h, img[idx].img_w, CV_64FC1, img[idx].image);
+
+            // center crop the image to fit the desired output image size
+            if ((img_w < img[idx].img_w) || (img_h < img[idx].img_h))
+            {
+                cv::Rect roi((img[idx].img_w - img_w) >> 1, (img[idx].img_h - img_h) >> 1, img_h, img_w);
+                tmp_img = tmp_img(roi);
+            }
+
+            // add the weighted image to the existing fused images
             fused_img = fused_img + img[idx].weight * (img[idx].invert_img ? (1.0 - tmp_img) : tmp_img);
-        }
-    }
+          }
+    }   // end of for loop
 
     fused_img.convertTo(fused_img8_t, CV_8UC1, 255.0, 0.0);
 
