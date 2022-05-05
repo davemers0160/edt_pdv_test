9d788b888e5c5550d4cbb723204932ca3a5659c7
davemers0160 <davemers@iu.edu>
2022-05-05 13:10:46 -0400

    adding homography class usage

--- homography_lib/src/homography_main.cpp
+++ homography_lib/src/homography_main.cpp
@@ -20,7 +20,7 @@
 #include "homography_lib.h"
 #include "img_registration.h"
 //#include "ms_image_struct.h"
-
+#include "homography_class.h"
 
 std::atomic<bool> homography_complete = false;
 cv::Point2f initial_point, final_point;
@@ -190,6 +190,9 @@ int main(int argc, char** argv)
 
     int32_t index = (int32_t)floor(stack_size*.75);
 
+    homography ref_h(75);
+    homography img_h(20);
+
     //cv::minMaxLoc(ref_img_stack[index], &min_val, &max_val);
     //ref_img_stack[index].convertTo(ref_img, CV_64FC1, 1.0 / (max_val - min_val), -min_val/ (max_val - min_val));
 
@@ -247,32 +250,33 @@ int main(int argc, char** argv)
 
 
 
-    ref_rect = get_bounding_box(ref_img_stack[0], ref_img, 80, invert_img[0]);
-    img_rect = get_bounding_box(img_stack[0], img, 60, invert_img[1]);
+    //ref_rect = get_bounding_box(ref_img_stack[0], ref_img, 80, invert_img[0]);
+    //img_rect = get_bounding_box(img_stack[0], img, 60, invert_img[1]);
 
-    // Find homography
-    h = cv::findHomography(get_rect_corners(img_rect), get_rect_corners(ref_rect), cv::RANSAC);
-    
+    //// Find homography
+    //h = cv::findHomography(get_rect_corners(img_rect), get_rect_corners(ref_rect), cv::RANSAC);
+    //
 
-    cv::Mat ref_draw = cv::Mat::zeros(ref_img.size(), CV_8UC3);
+    //cv::Mat ref_draw = cv::Mat::zeros(ref_img.size(), CV_8UC3);
     //for (size_t i = 0; i < ref_contours.size(); i++)
     //{
     //    cv::Scalar color = cv::Scalar(255,255,255);
     //    cv::drawContours(ref_draw, ref_contours, (int)i, color, 1, cv::LINE_8, ref_h, 0);
     //}
-    cv::rectangle(ref_draw, ref_rect, cv::Scalar::all(255), 1, 8, 0);
+    //cv::rectangle(ref_draw, ref_rect, cv::Scalar::all(255), 1, 8, 0);
 
-    cv::Mat img_draw = cv::Mat::zeros(img.size(), CV_8UC3);
+    //cv::Mat img_draw = cv::Mat::zeros(img.size(), CV_8UC3);
     //for (size_t i = 0; i < img_cont.size(); i++)
     //{
     //    cv::Scalar color = cv::Scalar(255,255,255);
     //    cv::drawContours(img_draw, img_cont, (int)i, color, 1, cv::LINE_8, img_hr, 0);
     //}
-    cv::rectangle(img_draw, img_rect, cv::Scalar::all(255), 1, 8, 0);
+    //cv::rectangle(img_draw, img_rect, cv::Scalar::all(255), 1, 8, 0);
 
     //cv::Mat h2;
     //find_transformation_matrix(ref_draw, img_draw, h, img_matches);
 
+    /*
     while (key != 'q')
     {
 
@@ -328,7 +332,7 @@ int main(int argc, char** argv)
             break;
         }
     }
-    
+    */
     //find_transformation_matrix(get_gradient(layers[1]), get_gradient(layers[0]), h, img_matches);
     //cv::drawMatches(layers[0], alignment_points1, layers[1], alignment_points2, matches, img_matches);
 
@@ -343,9 +347,8 @@ int main(int argc, char** argv)
 
     for (idx = 0; idx < stack_size; ++idx)
     {
-
+        /*
         ref_rect = get_bounding_box(ref_img_stack[idx], ref_img, 80, invert_img[0]);
-        //img_rect = get_bounding_box(img_stack[idx], img, 90, invert_img[1]);
         img_rect = get_bounding_box2(img_stack[idx], img, 25, invert_img[1]);
 
         cv::rectangle(ref_img, ref_rect, cv::Scalar::all(255), 1, 8, 0);
@@ -364,6 +367,23 @@ int main(int argc, char** argv)
         //cv::transpose(img, img);
 
         cv::warpPerspective(img, tmp_img, h, ref_img.size());
+        */
+
+        cv::transpose(ref_img_stack[idx], ref_img);
+        cv::transpose(img_stack[idx], img);
+
+        ref_h.get_bounding_box(ref_img, ref_img, invert_img[0]);
+        img_h.get_bounding_box(img, img, invert_img[1]);
+
+
+        cv::rectangle(ref_img, ref_h.get_rect(), cv::Scalar::all(255), 1, 8, 0);
+        cv::rectangle(img, img_h.get_rect(), cv::Scalar::all(255), 1, 8, 0);
+
+
+        img_h.calc_homography_matrix(ref_h.get_rect());
+
+        img_h.transform_image(img, tmp_img, ref_img.size());
+
         cv::hconcat(ref_img, img, montage_img);
 
         //fused_img = weights[1] * (invert_img[1] ? (1.0 - tmp_img) : tmp_img) + weights[0] * ref_img;
@@ -375,6 +395,7 @@ int main(int argc, char** argv)
         //montage_vec.push_back(montage_img);
         montage_vec[idx] = montage_img;
         key = cv::waitKey(0);
+        std::cout << "index: " << idx << std::endl;
 
         switch (key)
         {
